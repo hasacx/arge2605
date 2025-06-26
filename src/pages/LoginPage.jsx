8e06e494863400b554bc5ffdd513438700c6112e
hasacx <108523365+hasacx@users.noreply.github.com>
2025-06-26 10:03:29 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import { Box, Container, TextField, Button, Typography, Paper, Snackbar } from '
 import MuiAlert from '@mui/material/Alert'
 import { useFirebase } from '../firebase/FirebaseContext'
 
-function LoginPage() {
+function LoginPage() {zzzzzz11z1z1z1z1z
   const navigate = useNavigate()
   const { login } = useFirebase()
 
